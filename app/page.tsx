85c54a5d20f9b8c335a4445cbea50a2c016a7bd3
wyatthlang <wyattlang2001@gmail.com>
2025-02-01 15:13:14 -0500

    LPW-9: added projects grid

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,19 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 
-import { ModeToggle } from "@/components/theme-toggle";
+import { ModeToggle } from "@/components/theme-toggle"
+import { ProjectGrid } from "@/components//project-grid"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ModeToggle/>
+        <ModeToggle />
         <h1>Langhavens</h1>
         <div>
           <p>Building trading algorithms as a platform for personal development.</p>
         </div>
-        <h2>In Development</h2>
-        <div>
-          <p>Schwab API Java Client SDK</p>
-          <p>An open-source Java client for the Schwab Market Data developer API. This implementation features a reactive, non-blocking client as well as a traditional, blocking client.</p>
-        </div>
-        <div>
-          <p>Generative AI Market Day Recap Newsletter</p>
-          <p>A free newsletter generating daily recaps on the financial markets, powered by generative AI.</p>
-        </div>
-        <h2>In the Backlog</h2>
-        <div>
-          <p>Langhavens Securities Master</p>
-          <p>A securities master translates adjusted prices and standardizes identifiers for securities. Corporate actions like stock splits and symbol name changes create inconsistencies when looking back in time to compare financial data. A securities master keeps track of all these changes and transforms the data into a normalized format with respect to time.</p>
-        </div>
-        <div>
-          <p>Backtest Engine</p>
-          <p>Past performance is no guarantee of future results, but back-testing can provide some indication of an algorithm&apos;s forecasted returns. I want to write my own implementation of a backtesting engine because so many algorithmic traders use them, and I want to understand the nitty-gritty details of how they work.</p>
-        </div>
-        <div>
-          <p>Black-box Policy Proximal Optimization Algorithm</p>
-          <p>A machine learning algorithm for trading with lots of indicators. It&apos;s not expected to be effective, but it is a great opportunity for applied research.</p>
-        </div>
+        <ProjectGrid />
         <div>
           <h2>F.A.Q.</h2>
           <Accordion type="single" collapsible>
