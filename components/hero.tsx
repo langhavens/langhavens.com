c892b11e36b1956329e5a1937b965f09c09a948c
wyatthlang <wyattlang2001@gmail.com>
2025-02-02 16:47:23 -0500

    LPW-10: added hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,17 @@
 "use client"
 
 import * as React from "react"
-
 import { FlipWords } from "@/components/ui/flip-words"
 
 export function Hero() {
-  const words = ["A Quixotic", "A Sisyphean", "An unattainable", "A constantly evolving"]
+  const words = ["A Quixotic", "A continuous", "An endless", "A Sisyphean", "An unceasing", "A constantly evolving"]
 
   return (
-    <div className="relative h-screen">
-      <h1 className="text-5xl mb-5">Langhavens</h1>
-      <p className="text-3xl"><FlipWords words={words} /><br/>mission drives perpetual learning.</p>
+    <div className="relative h-[40rem] flex items-center justify-center w-full group">
+        <p className="text-2xl px-4 md:text-4xl lg:text-5xl max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto">
+          <FlipWords words={words} /><br />
+          mission drives <b>perpetual learning</b>.
+        </p>
     </div>
-  )
+  );
 }
