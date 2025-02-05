dafdaabf2849cbdd29165308218b074d6b136b53
wyatthlang <wyattlang2001@gmail.com>
2025-02-05 03:35:06 -0500

    slight change to hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,13 +4,13 @@ import * as React from "react"
 import { FlipWords } from "@/components/ui/flip-words"
 
 export function Hero() {
-  const words = ["A Quixotic", "A continuous", "An endless", "A Sisyphean", "An unceasing", "A constantly evolving"]
+  const words = ["A Quixotic", "An impossible", "A continuous", "An endless", "A Sisyphean", "An unceasing", "A constantly evolving"]
 
   return (
     <div className="flex items-center justify-center w-full group">
         <p className="text-2xl px-4 md:text-4xl lg:text-5xl max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto">
           <FlipWords words={words} /><br />
-          mission drives <b>perpetual learning</b>.
+          mission creates <b>perpetual learning</b> opportunities.
         </p>
     </div>
   );
